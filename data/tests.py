66fd8cd3330576697462475cd7474be917c820ab
fargus plumdoodle <dont@worry.about.it>
2019-04-08 12:21:35 -0700

    Added tests for  - creating games  - attempting t ocreate games wrongly  - deleting turns  - deleting games

--- data/tests.py
+++ data/tests.py
@@ -1,53 +1,23 @@
 # from django.test import TestCase
+from unittest import TestCase
+from django.contrib.auth.models import User
+from data.models import Game, Turn
 import random
 import requests
 import json
 
-DATA_HOST = 'http://localhost:8000'
 
-game = {
-    "you": {
-        "health": 98,
-        "body": [
-            {
-                "y": 3,
-                "x": 14
-            },
-            {
-                "y": 4,
-                "x": 14
-            },
-            {
-                "y": 4,
-                "x": 13
-            }
-        ],
-        "name": "SekhnetSnek",
-        "id": "13e7d710-dfb9-45c2-919d-5dacbf609686"
-    },
-    "turn": 0,
-    "board": {
-        "snakes": [
-            {
-                "health": 98,
-                "body": [
-                    {
-                        "y": 4,
-                        "x": 11
-                    },
-                    {
-                        "y": 3,
-                        "x": 11
-                    },
-                    {
-                        "y": 3,
-                        "x": 12
-                    }
-                ],
-                "name": "Son of Robosnake",
-                "id": "213cd80c-ef75-404c-968b-db2733ac6fbe"
-            },
-            {
+'''
+Assertions trigger when the statement is FALSE... you fool
+'''
+
+
+class TestGameEndpoint(TestCase):
+    def setUp(self):
+        # setting up game info
+        self.DATA_HOST = 'http://localhost:8000'
+        self.game = {
+            "you": {
                 "health": 98,
                 "body": [
                     {
@@ -65,114 +35,205 @@ game = {
                 ],
                 "name": "SekhnetSnek",
                 "id": "13e7d710-dfb9-45c2-919d-5dacbf609686"
+            },
+            "turn": 0,
+            "board": {
+                "snakes": [
+                    {
+                        "health": 98,
+                        "body": [
+                            {
+                                "y": 4,
+                                "x": 11
+                            },
+                            {
+                                "y": 3,
+                                "x": 11
+                            },
+                            {
+                                "y": 3,
+                                "x": 12
+                            }
+                        ],
+                        "name": "Son of Robosnake",
+                        "id": "213cd80c-ef75-404c-968b-db2733ac6fbe"
+                    },
+                    {
+                        "health": 98,
+                        "body": [
+                            {
+                                "y": 3,
+                                "x": 14
+                            },
+                            {
+                                "y": 4,
+                                "x": 14
+                            },
+                            {
+                                "y": 4,
+                                "x": 13
+                            }
+                        ],
+                        "name": "SekhnetSnek",
+                        "id": "13e7d710-dfb9-45c2-919d-5dacbf609686"
+                    }
+                ],
+                "width": 17,
+                "height": 10,
+                "food": [
+                    {
+                        "y": 4,
+                        "x": 16
+                    },
+                    {
+                        "y": 0,
+                        "x": 5
+                    },
+                    {
+                        "y": 7,
+                        "x": 8
+                    },
+                    {
+                        "y": 0,
+                        "x": 0
+                    },
+                    {
+                        "y": 0,
+                        "x": 7
+                    },
+                    {
+                        "y": 4,
+                        "x": 1
+                    },
+                    {
+                        "y": 1,
+                        "x": 5
+                    },
+                    {
+                        "y": 1,
+                        "x": 12
+                    },
+                    {
+                        "y": 9,
+                        "x": 4
+                    },
+                    {
+                        "y": 9,
+                        "x": 5
+                    }
+                ]
+            },
+            "game": {
+                "id": "NewTestGame1"
             }
-        ],
-        "width": 17,
-        "height": 10,
-        "food": [
-            {
-                "y": 4,
-                "x": 16
-            },
-            {
-                "y": 0,
-                "x": 5
-            },
-            {
-                "y": 7,
-                "x": 8
-            },
-            {
-                "y": 0,
-                "x": 0
-            },
-            {
-                "y": 0,
-                "x": 7
-            },
-            {
-                "y": 4,
-                "x": 1
-            },
-            {
-                "y": 1,
-                "x": 5
-            },
-            {
-                "y": 1,
-                "x": 12
-            },
-            {
-                "y": 9,
-                "x": 4
-            },
-            {
-                "y": 9,
-                "x": 5
-            }
-        ]
-    },
-    "game": {
-        "id": "NewTestGame1"
-    }
-}
+        }
+        self.gid = 't_' + str(random.choice(range(40)))
+        self.game['game']['id'] = self.gid
 
-game['game']['id'] = 't_' + str(random.choice(range(40)))
+        # setting up user info
+        self.username = 'fargusD'
+        self.password = '1234asdf'
 
-
-def api_test():
-    def send_data(option, data):
+    def send_data(self, option, data):
         content = json.dumps({
             'option': option,
             'data': data
         })
-        r = requests.post(url=DATA_HOST + "/games/", data=content, auth=('fargusD', '1234asdf'))
-        if r.status_code != 200:
-            raise ValueError('Host returned a non 200 status code: ' + str(r.status_code))
+        r = requests.post(url=self.DATA_HOST + "/games/", data=content, auth=(self.username, self.password))
+        return r
 
-    # start testing
-    send_data('start', game)
+    def create_game(self, gid):
+        # creating new gid for this game
+        previous_gid = self.game['game']['id']
+        self.gid = gid
+        self.game['game']['id'] = gid
 
-    # sending turns
-    for i in range(3):
-        game['turn'] += 1
-        send_data('turn', game)
+        # --- set up ----
+        self.send_data('start', self.game)
 
-    # ending game
-    game['turn'] += 1
-    send_data('end', game)
+        # sending turns
+        for i in range(3):
+            self.game['turn'] += 1
+            self.send_data('turn', self.game)
 
-# r = requests.delete(url=DATA_HOST + "/games", data=json.dumps({'gid': game['game']['id']}), auth=('fargusD', '1234asdf'))
-# if r.status_code != 200:
-#     raise ValueError('Host returned a non 200 status code: ' + r.status_code)
-#
-# def snake_test():
-#
-#     def send_data(option, data):
-#         content = json.dumps(data)
-#         r = requests.post(url=DATA_HOST + "/SekhnetSnake/" + option, data=content)
-#         if r.status_code != 200:
-#             raise ValueError('Host returned a non 200 status code: ' + r.status_code)
-#         print(r.content.decode('utf-8'))
-#
-#     # start testing
-#     send_data('start', game)
-#
-#     # sending turns
-#     for i in range(3):
-#         game['turn'] += 1
-#         send_data('move', game)
-#
-#     # ending game
-#     game['turn'] += 1
-#     send_data('end', game)
-#
-#     # r = requests.delete(url=DATA_HOST + "/games", data=json.dumps({'gid': game['game']['id']}), auth=('fargusD', '1234asdf'))
-#     # if r.status_code != 200:
-#     #     raise ValueError('Host returned a non 200 status code: ' + r.status_code)
-#     # print(r.content)
+        # ending game
+        self.game['turn'] += 1
+        self.send_data('end', self.game)
 
-if __name__ == '__main__':
-    api_test()
-# snake_test()
+        # cleaning up
+        self.gid = previous_gid
+        self.game['game']['id'] = previous_gid
+
+    def test_create_game_properly(self):
+        # --- set up ----
+        self.send_data('start', self.game)
+
+        # sending turns
+        for i in range(3):
+            self.game['turn'] += 1
+            self.send_data('turn', self.game)
+
+        # ending game
+        self.game['turn'] += 1
+        self.send_data('end', self.game)
+
+        # --- asserting the funnel cakes ----
+        game = Game.objects.filter(gid=self.gid).first()
+
+        assert game is not None  # Checking if game was created
+        assert game.width == self.game['board']['width']
+        assert game.height == self.game['board']['height']
+
+    def test_delete_endpoint(self):
+        # ---- creating temp game
+        gid = 'temp_12345'
+        self.create_game(gid)
+
+        # ---- test deleting turns
+        game = Game.objects.get(gid=gid)
+        num_turns = len(Turn.objects.filter(game=game))
+
+        for x in range(num_turns):
+            content = json.dumps({
+                'gid': gid,
+                'turn': x + 1
+            })
+            r = requests.delete(url=self.DATA_HOST + "/games/", data=content, auth=(self.username, self.password))
+            assert r.status_code == 200
+
+        assert len(Turn.objects.filter(game=game)) == 0  # should be no remaining games
+
+        # ---- test deleting game
+        content = json.dumps({
+            'gid': gid
+        })
+        r = requests.delete(url=self.DATA_HOST + "/games/", data=content, auth=(self.username, self.password))
+
+        assert r.status_code == 200  # status code should be 200 when we delete the game we just made
+
+    def test_send_turns_to_a_nonexistent_game(self):
+        # --- set up ----
+        # self.send_data('start', self.game)
+
+        # making invalid gid
+        self.game['game']['id'] = self.gid + 'asdf'
+
+        # sending turns
+        for i in range(3):
+            self.game['turn'] += 1
+            r = self.send_data('turn', self.game)
+            assert r.status_code != 200  # status code shouldn't be 200 after we sent invalid data
+
+        # ending game
+        self.game['turn'] += 1
+        r = self.send_data('end', self.game)
+        assert r.status_code != 200  # status code shouldn't be 200 after we sent invalid data
+
+    def tearDown(self):
+        Game.objects.filter(gid='temp_12345').delete()
+
+        # deleting test game object
+        if len(Game.objects.filter(gid=self.gid)) > 0:
+            Game.objects.filter(gid=self.gid).delete()
+
 
