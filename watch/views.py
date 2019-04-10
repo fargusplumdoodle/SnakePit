e310eae546d139fee2913575c41a7539d066a243
Isaac Thiessen <ike.thiessen0@gmail.com>
2019-04-10 14:09:05 -0700

    Fixed the way urls are called

--- watch/views.py
+++ watch/views.py
@@ -7,8 +7,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
 
-# @login_required(login_url='accounts/login/')
+@login_required(login_url='accounts/login/')
 def index(request):
-    content = {'api_url': request.build_absolute_uri('/') + 'games/snake'}
+    content = {'api_url': request.build_absolute_uri('/')}
     return render(request, 'watch/watch_games.html', content)
 
